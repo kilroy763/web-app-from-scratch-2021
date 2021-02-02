d5a79bec52c955c66e3464e12c5bfcba62858768
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2021-02-02 16:15:51 +0100

    xx

--- js/script.js
+++ js/script.js
@@ -3,15 +3,19 @@ const cors = 'https://api.themoviedb.org/3/tv/1396';
 const key = 'ae3a232f0096c607ad590f0ec850e635';
 const url = `${cors}?api_key=${key}`;
 
+
 fetch (url) 
     .then(response => response.json())
     .then(data => render(data));
             
+
 function render(putData){
     let data = putData;
     name(data);
     overview(data);
     poster_path(data);
+    sNaam(data);
+    vote_average(data);
     console.log("data", data)
     console.log(data.name);
 
@@ -19,22 +23,26 @@ function render(putData){
 
 
 function poster_path(data){
-
 const info = document.getElementById('info')
-
 const posters = [data.seasons[1].poster_path, data.seasons[2].poster_path, data.seasons[3].poster_path, data.seasons[4].poster_path, data.seasons[5].poster_path]
-
 //https://www.youtube.com/watch?v=e0ihEHxd6vI&ab_channel=dcode
 for (let poster of posters) {
     let nieuwposter = document.createElement('img')
     nieuwposter.src = img + poster;
     info.appendChild(nieuwposter)
-
+}
 }
 
+function sNaam(data){
+    const info = document.getElementById('info')
+    const naam = [data.seasons[1].name, data.seasons[2].name, data.seasons[3].name, data.seasons[4].name, data.seasons[5].name]
+    //https://www.youtube.com/watch?v=e0ihEHxd6vI&ab_channel=dcode
+    for (let namen of naam) {
+        let sNaam = document.createElement('p')
+        sNaam.textContent = namen ;
+        info.appendChild(sNaam)
+    }
 }
-
-
 
 
 
@@ -43,7 +51,13 @@ function name(data){
     cont.innerHTML = data.name
 }
 
+
 function  overview(data){
     let cont = document.getElementById('overview')
     cont.innerHTML = data.overview
+}
+
+function vote_average(data){
+    let cont = document.getElementById('cijfer')
+    cont.innerHTML = data.vote_average
 }
