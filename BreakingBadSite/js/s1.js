2fb77f68a4cce76fd0a820a8b3020af2c60c9d7b
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2021-02-05 09:58:42 +0100

    mappen structuur aangepast

--- BreakingBadSite/js/s1.js
+++ BreakingBadSite/js/s1.js
@@ -1,13 +1,12 @@
-const image = "https://image.tmdb.org/t/p/w500" ;
+const image = "https://image.tmdb.org/t/p/w500" ; // standaard url voor een image
 
 
-// GET REQUEST
-const movies = fetch(`https://api.themoviedb.org/3/tv/1396/season/1?api_key=ae3a232f0096c607ad590f0ec850e635&language=en-US  `)
+const movies = fetch(`https://api.themoviedb.org/3/tv/1396/season/1?api_key=ae3a232f0096c607ad590f0ec850e635&language=en-US  `) // api aanroepen
                 .then(response => response.json())
                 .then(data => render(data));
             
                 
-function render(putData){
+function render(putData){ //nodige elementen oproepen
     let data = putData;
     console.log("this is the data", data)
     name(data);
@@ -16,19 +15,18 @@ function render(putData){
     still_path(data);
     epname(data)
     console.log(data.name);
-
 }
 
 
 function name(data){
     let cont = document.getElementById('title')
     cont.innerHTML = data.name
-}
+}   // titel van serie oproepen
 
 function  overview(data){
     let cont = document.getElementById('overview')
     cont.innerHTML = data.overview
-}
+} // plot van serie oproepen
 
 
 
@@ -42,37 +40,29 @@ function poster_path(data) {
     article.id = "image1";
     article.appendChild(img);
     cont.appendChild(article);
-}
+} //poster oproepen
 
 function still_path(data){
-
     const info = document.getElementById('info')
-    
     const posters = [data.episodes[0].still_path, data.episodes[1].still_path, data.episodes[2].still_path, data.episodes[3].still_path, data.episodes[4].still_path]
-    
-    //https://www.youtube.com/watch?v=e0ihEHxd6vI&ab_channel=dcode
     for (let poster of posters) {
         let nieuwposter = document.createElement('img')
         nieuwposter.src = image + poster;
         info.appendChild(nieuwposter)
-    
     }
-    
     }
+    //screens van afleveringen oproepen
+    //bron:https://www.youtube.com/watch?v=e0ihEHxd6vI&ab_channel=dcode
 
     function epname(data){
-
         const info = document.getElementById('info')
-        
         const naam = [data.episodes[0].name, data.episodes[1].name, data.episodes[2].name, data.episodes[3].name, data.episodes[4].name]
-        
-        //https://www.youtube.com/watch?v=e0ihEHxd6vI&ab_channel=dcode
         for (let namen of naam) {
             let epnaam = document.createElement('p')
             epnaam.textContent = namen ;
             info.appendChild(epnaam)
-        
         }
-        
         }
+    //namen van afleveringen oproepen
+    //bron:https://www.youtube.com/watch?v=e0ihEHxd6vI&ab_channel=dcode
     
--- BreakingBadSite/js/script.js
+++ BreakingBadSite/js/script.js
@@ -1,15 +1,15 @@
-const img = "https://image.tmdb.org/t/p/w500" ;
-const cors = 'https://api.themoviedb.org/3/tv/1396';
-const key = 'ae3a232f0096c607ad590f0ec850e635';
-const url = `${cors}?api_key=${key}`;
+const img = "https://image.tmdb.org/t/p/w500" ; //algemene link van de images 
+const cors = 'https://api.themoviedb.org/3/tv/1396'; //algemene link van de API
+const key = 'ae3a232f0096c607ad590f0ec850e635'; //key van de API
+const url = `${cors}?api_key=${key}`; //samenvoegen van de link en key
 
 
 fetch (url) 
     .then(response => response.json())
     .then(data => render(data));
-            
+    //oproepen API
 
-function render(putData){
+function render(putData){ //nodige elementen oproepen
     let data = putData;
     name(data);
     overview(data);
@@ -18,46 +18,46 @@ function render(putData){
     vote_average(data);
     console.log("data", data)
     console.log(data.name);
-
 }
 
 
 function poster_path(data){
 const info = document.getElementById('info')
 const posters = [data.seasons[1].poster_path, data.seasons[2].poster_path, data.seasons[3].poster_path, data.seasons[4].poster_path, data.seasons[5].poster_path]
-//https://www.youtube.com/watch?v=e0ihEHxd6vI&ab_channel=dcode
 for (let poster of posters) {
     let nieuwposter = document.createElement('img')
     nieuwposter.src = img + poster;
     info.appendChild(nieuwposter)
 }
 }
+    //poster van de seizoenen oproepen
+    //bron:https://www.youtube.com/watch?v=e0ihEHxd6vI&ab_channel=dcode
 
 function sNaam(data){
     const info = document.getElementById('info')
     const naam = [data.seasons[1].name, data.seasons[2].name, data.seasons[3].name, data.seasons[4].name, data.seasons[5].name]
-    //https://www.youtube.com/watch?v=e0ihEHxd6vI&ab_channel=dcode
     for (let namen of naam) {
         let sNaam = document.createElement('p')
         sNaam.textContent = namen ;
         info.appendChild(sNaam)
     }
 }
-
+    //namen van de seizoenen oproepen
+    //bron:https://www.youtube.com/watch?v=e0ihEHxd6vI&ab_channel=dcode
 
 
 function name(data){
     let cont = document.getElementById('title')
     cont.innerHTML = data.name
-}
+} // titel van serie oproepen
 
 
 function  overview(data){
     let cont = document.getElementById('overview')
     cont.innerHTML = data.overview
-}
+} // plot van serie oproepen
 
 function vote_average(data){
     let cont = document.getElementById('cijfer')
     cont.innerHTML = data.vote_average
-}
+} // cijfer van serie oproepen
